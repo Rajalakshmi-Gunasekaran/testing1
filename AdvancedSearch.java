ec782be67066a8cfeffa8f9d5fd808cf5a490412
Raji Anand <84066539+Rajalakshmi-Gunasekaran@users.noreply.github.com>
2021-07-02 08:53:27 +0100

    Update AdvancedSearch.java

--- AdvancedSearch.java
+++ AdvancedSearch.java
@@ -16,13 +16,15 @@ public class AdvancedSearch extends BaseStep{
     }
 
     @When("^I click on outlet type and enter outlet type as \"(.*?)\"$")
-    public void i_click_on_outlet_type_and_enter_outlet_type_as(String outletType) {
+    public void i_click_on_outlet_type_and_enter_outlet_type_as(String outletType)
+    {
         pauseFor(5);
-            advancedSearchPage.enterOutletType(outletType);
+        advancedSearchPage.enterOutletType(outletType);
     }
 
     @When("^I Click on outlet name and enter outlet name as \"(.*?)\"$")
-    public void i_Click_on_outlet_name_and_enter_outlet_name_as(String outletName){
+    public void i_Click_on_outlet_name_and_enter_outlet_name_as(String outletName)
+    {
 
         pauseFor(5);
         advancedSearchPage.enterOutletName(outletName);
@@ -30,40 +32,47 @@ public class AdvancedSearch extends BaseStep{
     }
 
     @Then("^I should see the Journalist list for the selected outlet$")
-    public void i_should_see_the_Journalist_list_for_the_selected_outlet() {
-advancedSearchPage.setValidateOutletSearch();
+    public void i_should_see_the_Journalist_list_for_the_selected_outlet()
+    {
+        advancedSearchPage.setValidateOutletSearch();
     }
     @When("^I click on Group-By icon$")
-    public void i_click_on_Group_By_icon() {
+    public void i_click_on_Group_By_icon()
+    {
         pauseFor(5);
        advancedSearchPage.clickGroupByIcon();
     }
 
     @When("^I choose country as Group-By$")
-    public void i_choose_country_as_Group_By(){
+    public void i_choose_country_as_Group_By()
+    {
         pauseFor(5);
         advancedSearchPage.clickCountryGroupBy();
     }
 
     @Then("^I should able to see the journalist list grouped by country$")
-    public void i_should_able_to_see_the_journalist_list_grouped_by_country() {
+    public void i_should_able_to_see_the_journalist_list_grouped_by_country() 
+    {
         pauseFor(5);
         advancedSearchPage.validateListGroupedByCountry();
     }
     @When("^I click on change info icon$")
-    public void i_click_on_change_info_icon(){
+    public void i_click_on_change_info_icon()
+    {
         pauseFor(5);
         advancedSearchPage.clickChangeInfo();
     }
 
     @When("^I choose info as Roxhill says$")
-    public void i_choose_info_as_Roxhill_says(){
+    public void i_choose_info_as_Roxhill_says()
+    {
         pauseFor(5);
        advancedSearchPage.chooseInfo();
     }
 
     @Then("^I should able to see the journalist list view by Roxhill says inside the tile$")
-    public void i_should_able_to_see_the_journalist_list_view_by_Roxhill_says_inside_the_tile() {
+    public void i_should_able_to_see_the_journalist_list_view_by_Roxhill_says_inside_the_tile() 
+    {
         pauseFor(5);
         advancedSearchPage.validateJournalistInfo();
     }
