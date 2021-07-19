be15a0ddc3a50692a254d116fa4c05894ff23475
Raji Anand <84066539+Rajalakshmi-Gunasekaran@users.noreply.github.com>
2021-07-19 13:30:35 +0100

    Update Runner.java

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,14 +6,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-  features = ".",tags = "@DeleteStream",
-  dryRun =false)
-  //plugin={"com.cucumber.listener.ExtentCucumberFormatter:target/TSR/tsr1.html"})
+  features = ".", dryRun =false,
+  plugin={"com.cucumber.listener.ExtentCucumberFormatter:target/TSR/tsr1.html"})
 public class Runner {
-   /*@AfterClass
+   @AfterClass
     public static void generateReport()
     {
         Reporter.loadXMLConfig("C:\\Users\\RajiAnand\\IdeaProjects\\roxhill\\testing\\src\\main\\resources\\reportConfig\\extent-config.xml");
-    }*/
+    }
 }
 
