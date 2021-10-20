56c8f10bc537f9c94b02a7d19680634239ce8f57
Rajalakshmi-Gunasekaran <raji.anand@roxhillmedia.com>
2021-10-20 16:57:54 +0100

    OutletProfile_added

--- src/test/java/stepDefinitions/Search.java
+++ src/test/java/stepDefinitions/Search.java
@@ -210,4 +210,248 @@ journoProfilePage.setValidateJournoRemovalFromList();
        wait(30);
        listsPage.setDeleteListBtn();
     }
+    @When("^I choose journo to add activity$")
+    public void i_choose_journo_to_add_activity() throws InterruptedException {
+        wait(30);
+        outletProfilePage.chooseJournoToAddActivity();
+    }
+
+    @When("^I click on add activity for the journo$")
+    public void i_click_on_add_activity_for_the_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickAddActivity();
+    }
+    @When("^I enter subject for an activity as \"([^\"]*)\"$")
+    public void i_enter_subject_for_an_activity_as(String enterSubject) throws InterruptedException {
+        wait(30);
+        outletProfilePage.setEnterSubjectForActivity(enterSubject);
+    }
+
+    @When("^I click on save button to add activity$")
+    public void i_click_on_save_button_to_add_activity() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickSaveBtn();
+    }
+
+    @When("^I click on activities tab$")
+    public void i_click_on_activities_tab() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickActivityTab();
+    }
+
+    @When("^I should see the activity added for the journo$")
+    public void i_should_see_the_activity_added_for_the_journo() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateActivityForJourno();
+    }
+
+    @Then("^I delete and confirm delete activity$")
+    public void i_delete_and_confirm_delete_activity() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickDeleteActivity();
+    }
+    @When("^I choose desk and click on it$")
+    public void i_choose_desk_and_click_on_it() throws InterruptedException {
+        wait(30);
+        outletProfilePage.chooseDeskForJournos();
+    }
+
+    @When("^I should see the journo for the selected desks$")
+    public void i_should_see_the_journo_for_the_selected_desks() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateJournoForChosenDesk();
+    }
+
+    @When("^I click on clear all button$")
+    public void i_click_on_clear_all_button() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickClearAll();
+    }
+
+    @Then("^I should see journos for all the desks$")
+    public void i_should_see_journos_for_all_the_desks() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateAllJournosForAllDesks();
+    }
+    @When("^I click on hide in group button$")
+    public void i_click_on_hide_in_group_button() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickHide();
+    }
+
+    @When("^I should see the journo hide in group$")
+    public void i_should_see_the_journo_hide_in_group() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateHideJourno();
+    }
+
+    @When("^I click on un hide in group button$")
+    public void i_click_on_un_hide_in_group_button() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickUnhide();
+    }
+
+    @Then("^I should see journos are un hide in group$")
+    public void i_should_see_journos_are_un_hide_in_group() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateUnHideJourno();
+    }
+    @When("^I click on select in group button$")
+    public void i_click_on_select_in_group_button() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickSelectInGroup();
+    }
+
+    @When("^I should see the journo selected in group$")
+    public void i_should_see_the_journo_selected_in_group() throws InterruptedException {
+        wait(30);
+        outletProfilePage.validateJournoSelectInGroup();
+    }
+
+    @When("^I click on deselect in group button$")
+    public void i_click_on_deselect_in_group_button() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickDesselectJourno();
+    }
+
+    @Then("^I should see journos are deselected in group$")
+    public void i_should_see_journos_are_deselected_in_group() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateJournoDeselect();
+    }
+    @When("^I click on add to favourites button$")
+    public void i_click_on_add_to_favourites_button() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickAddToFavouritesBtn();
+    }
+
+    @When("^I click on home button to go back to home page$")
+    public void i_click_on_home_button_to_go_back_to_home_page() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickHomeBtn();
+    }
+
+    @When("^I click on favourite outlets tab$")
+    public void i_click_on_favourite_outlets_tab() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickFavouritesOutletTab();
+    }
+
+    @Then("^I should see the outlet added to favourite outlets tab$")
+    public void i_should_see_the_outlet_added_to_favourite_outlets_tab() throws InterruptedException {
+        wait(30);
+        outletProfilePage.validateOutletAddedToFavourites();
+    }
+    @When("^I click on Quick search tab$")
+    public void i_click_on_Quick_search_tab() throws InterruptedException {
+        wait(30);
+        search.setClkQuickSearch();
+    }
+
+    @Then("^I should uncheck favourite button in outlet profile page$")
+    public void i_should_uncheck_favourite_button_in_outlet_profile_page() throws InterruptedException {
+     wait(30);
+     outletProfilePage.setClickAddToFavouritesBtn();
+    }
+    @When("^I click on sector for the chosen journo$")
+    public void i_click_on_sector_for_the_chosen_journo() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setClickOnSectorInJournoTile();
+    }
+
+    @Then("^I should see all the available sector for that particular journo$")
+    public void i_should_see_all_the_available_sector_for_that_particular_journo() throws InterruptedException {
+       wait(30);
+       outletProfilePage.validateSectorResultsInJournoTile();
+    }
+    @When("^I click on coverage for the chosen journo$")
+    public void i_click_on_coverage_for_the_chosen_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickCoverage();
+    }
+
+    @Then("^I should see all the available topic coverage for that particular journo$")
+    public void i_should_see_all_the_available_topic_coverage_for_that_particular_journo() throws InterruptedException {
+       wait(30);
+       outletProfilePage.setValidateCoverageLinkResult();
+    }
+    @When("^I click on tweets for the chosen journo$")
+    public void i_click_on_tweets_for_the_chosen_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickTweetInJournotile();
+    }
+
+    @Then("^I should see all the available tweets for that particular journo$")
+    public void i_should_see_all_the_available_tweets_for_that_particular_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setValidateTweetLinkResult();
+    }
+    @When("^I click on instagram for the chosen journo$")
+    public void i_click_on_instagram_for_the_chosen_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickInstagramInJournoTile();
+    }
+
+    @Then("^I should see all the available Instagram results for that particular journo$")
+    public void i_should_see_all_the_available_Instagram_results_for_that_particular_journo() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setValidateInstagramResults();
+    }
+    @When("^I click on alerts tab$")
+    public void i_click_on_alerts_tab() throws InterruptedException {
+        wait(30);
+        outletProfilePage.setClickAlertsTab();
+    }
+
+    @When("^I enter quick find by keyword as \"([^\"]*)\"$")
+    public void i_enter_quick_find_by_keyword_as(String keyword) throws InterruptedException {
+      wait(30);
+      outletProfilePage.setEnterQuickFindByKeyword(keyword);
+    }
+
+    @Then("^I should see the result for the search keyword$")
+    public void i_should_see_the_result_for_the_search_keyword() throws InterruptedException {
+     wait(30);
+     outletProfilePage.validateKeywordSearchResult();
+    }
+    @When("^I enter journo name to search as \"([^\"]*)\"$")
+    public void i_enter_journo_name_to_search_as(String searchName) throws InterruptedException {
+        wait(30);
+        outletProfilePage.setEnterSearchByNameJournoSearch(searchName);
+    }
+
+    @Then("^I should see the searched journo at the top$")
+    public void i_should_see_the_searched_journo_at_the_top() throws InterruptedException {
+        wait(30);
+        outletProfilePage.validateSearchByNameJournoResult();
+    }
+    @When("^I click on group by button$")
+    public void i_click_on_group_by_button() throws InterruptedException {
+      wait(30);
+      outletProfilePage.setClickGroupByButton();
+    }
+
+    @When("^I click on Un-group option from the list$")
+    public void i_click_on_Un_group_option_from_the_list() throws InterruptedException {
+     wait(30);
+     outletProfilePage.setClickUnGroupOption();
+    }
+
+    @When("^I should see the results for un grouping$")
+    public void i_should_see_the_results_for_un_grouping() throws InterruptedException {
+     wait(30);
+     outletProfilePage.validateUnGroupresults();
+    }
+
+    @When("^I click on sort direction button$")
+    public void i_click_on_sort_direction_button() throws InterruptedException {
+     wait(30);
+     outletProfilePage.setClickSortDirection();
+    }
+
+    @Then("^I should see the results for sorting direction$")
+    public void i_should_see_the_results_for_sorting_direction() throws InterruptedException {
+wait(30);
+outletProfilePage.validateJournoSortDirection();
+    }
 }
