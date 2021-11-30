9f8d6cce697d50d74e5c3a9b8ce280290caba57d
Raji Anand <84066539+Rajalakshmi-Gunasekaran@users.noreply.github.com>
2021-11-30 23:21:43 +0000

    Update TestRunner.java

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = ".", dryRun = false,tags = "@functional62,@functional60",
+        features = ".", dryRun = false,
         plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/TSR/tsr3.html"})
  public class TestRunner {
       @AfterClass
